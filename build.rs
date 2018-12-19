b1732d90a3f388aa58c3f0a23c97acf809f8242b
Riey <creeper844@gmail.com>
2018-12-19 19:23:20 +0900

    Add qni_api.rs

--- build.rs
+++ build.rs
@@ -1,23 +1,16 @@
 use protoc_rust::Customize;
-use std::process;
+use std::path;
 
 fn main() {
-    process::Command::new("git").args(
-        &[
-            "clone",
-            "--depth 1",
-            "https://github.com/Riey/qni-api-protos",
-            "api",
-        ]
-    ).spawn().expect("git");
-
-    protoc_rust::run(protoc_rust::Args {
-        out_dir: "src/protos",
-        input: &["api/qni-api.proto"],
-        includes: &["api"],
-        customize: Customize {
-            ..Default::default()
-        },
-    })
-    .expect("protoc");
+    if path::Path::new("api/qni-api.proto").exists() {
+        protoc_rust::run(protoc_rust::Args {
+            out_dir: "src/protos",
+            input: &["api/qni-api.proto"],
+            includes: &["api"],
+            customize: Customize {
+                ..Default::default()
+            },
+        })
+            .expect("protoc");
+    }
 }
