207d8d54dbd9e8cdb5f8aea6c383fb4afe1a6e89
Riey <creeper844@gmail.com>
2018-12-19 19:18:30 +0900

    Add api-proto clone in build

--- build.rs
+++ build.rs
@@ -1,6 +1,16 @@
 use protoc_rust::Customize;
+use std::process;
 
 fn main() {
+    process::Command::new("git").args(
+        &[
+            "clone",
+            "--depth 1",
+            "https://github.com/Riey/qni-api-protos",
+            "api",
+        ]
+    ).spawn().expect("git");
+
     protoc_rust::run(protoc_rust::Args {
         out_dir: "src/protos",
         input: &["api/qni-api.proto"],
